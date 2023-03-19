476f124416df273a04f8fcbce9daf51b5ecb119d
davidson <davidson031@hotmail.com>
2023-03-18 21:51:40 -0300

    setup mongodb e primeiro model

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+
+//CORS
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+
+//Diretório de upload de imagens
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+
+//DB
+require("./config/db.js");
+
+
 //rotas
 const router = require("./routes/Router");
 app.use(router);
+
+
 
 app.listen(port, () => console.log(`Backend rolando na porta ${ port }`));
