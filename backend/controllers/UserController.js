9e90a1c32bd95964ab04043af1e0b31bd8ebb1c4
davidson <davidson031@hotmail.com>
2023-03-19 19:53:28 -0300

    criando usuario com dados recebidos na request

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -16,7 +16,40 @@ const generateToken = (id) => {
 //register user (and sign-in afterwards)
 const register = async (req, res) => {
 
-    res.send("REGISTREI...");
+
+    const { name, email, password } = req.body;
+
+    //see if user exists
+    const user = await User.findOne( { email });
+
+    if(user){
+        res.status(422).json({ errors: "Por favor, utilize outro e-mail" });
+        return;
+    } 
+
+    //generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordSalt = await bcrypt.hash(password, salt);
+
+
+    //criar user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+
+    //check if user was created and return token if so
+    if(!newUser){
+        res.status(422).json({ errors: "Houve um erro, favor tente mais tarde "});
+        return;
+    };
+
+    res.status(201).json({
+        id: newUser._id,
+        token: generateToken(newUser._id)
+    })
 }
 
 
