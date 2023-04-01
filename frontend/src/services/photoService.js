b56f6507d2b7a4fa0f5f79254e592e4fc378060a
davidson <davidson031@hotmail.com>
2023-04-01 00:45:37 -0300

    service e slice para remocao de fotos

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -38,9 +38,25 @@ const getUserPhotos = async (id, token) => {
     }
 }
 
+const deletePhoto = async (id, token) => {
+
+    const config = requestConfig("DELETE", null, token);
+
+    try {
+
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const photoService = {
     publishPhoto,
-    getUserPhotos
+    getUserPhotos,
+    deletePhoto
 };
 
 
