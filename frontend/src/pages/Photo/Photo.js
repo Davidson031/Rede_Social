c70f3e009a38c5ce6c8ba8896e79738bcef63a9b
davidson <davidson031@hotmail.com>
2023-04-03 12:36:07 -0300

    externalizando limpeza de mensagens de feedback  em um custom hook

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -11,21 +11,19 @@ import PhotoItem from "../../components/PhotoItem";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 //redux
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, resetMessage } from "../../slices/photoSlice";
 import LikeContainer from "../../components/LikeContainer";
 
 const Photo = () => {
-  
-    const handleLike = () => {
 
-      dispatch(like(photo._id));
-      
-    }
     const {id} = useParams();
 
     const dispatch = useDispatch();
+
+    const resetMessage = useResetComponentMessage(dispatch);
 
     const { user } = useSelector((state) => state.auth);
 
@@ -40,10 +38,21 @@ const Photo = () => {
     if(loading){
         return <p>Carregando...</p>
     }
+
+    const handleLike = () => {
+
+      dispatch(like(photo._id));
+      resetMessage();
+      
+    }
   return (
     <div id="photo">
         <PhotoItem photo={ photo }/>
         <LikeContainer photo ={ photo } user = { user } handleLike={ handleLike }/>
+        <div className="message-container">
+            { error && <Message msg={ error } type= "error" />}
+            { message && <Message msg={ message } type= "success" />}
+        </div>
     </div>
   )
 }
