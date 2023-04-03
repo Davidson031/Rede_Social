1b9acc853649b83e5f7b8df6357b3e56c065ef23
davidson <davidson031@hotmail.com>
2023-04-03 14:23:34 -0300

    service e slice de comentarios nas fotos

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -101,6 +101,21 @@ export const like = createAsyncThunk(
     }
 );
 
+export const comment = createAsyncThunk(
+    "photo/comment",
+    async (photoData, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.comment({ comment: photoData.comment }, photoData.id, token);
+
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
+)
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -195,6 +210,17 @@ export const photoSlice = createSlice({
                 }
                 return photo;
             })
+            state.message = action.payload.message
+        }).addCase(comment.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        }).addCase(comment.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            
+            state.photo.comments.push(action.payload.comment);
+            
             state.message = action.payload.message
         })
     }
